de9006da55236d7de11c9c3b4bd5c17d880297e2
ZhouxuanXia <josenxia@163.com>
2026-06-22 08:51:01 +0800

    Finish SAM_LoRA with mask decoder 5-fold training.

--- train.py
+++ train.py
@@ -8,6 +8,8 @@
 对于fold2 实验编号是num_exp=11
 
 对于fold3 实验编号是num_exp=12
+
+对于fold4 实验编号是num_exp=13
 """
 import os
 import numpy as np
@@ -62,7 +64,7 @@ CUDA_VISIBLE_DEVICES=? nohup poetry run python inference_plots.py
 """
 
 # 实验编号
-num_exp = 12
+num_exp = 13
 
 exp_dir = os.path.join("/mnt/hdd2/task2/sam_lora", f"exp_{num_exp}")
 os.makedirs(exp_dir, exist_ok=True)
@@ -139,7 +141,7 @@ processor = Samprocessor(model)
 
 # Create train dataloader
 # train_ds = DatasetSegmentation(config_file, processor, mode="train1")
-train_ds = DatasetSegmentation(config_file, processor, mode="train4")
+train_ds = DatasetSegmentation(config_file, processor, mode="train5")
 train_dataloader = DataLoader(train_ds, 
                               batch_size=config_file["TRAIN"]["BATCH_SIZE"], 
                               shuffle=True, 
@@ -147,7 +149,7 @@ train_dataloader = DataLoader(train_ds,
 
 # Create val dataloader
 # val_ds = DatasetSegmentation(config_file, processor, mode="val1")
-val_ds = DatasetSegmentation(config_file, processor, mode="val4")
+val_ds = DatasetSegmentation(config_file, processor, mode="val5")
 val_dataloader = DataLoader(val_ds, 
                             batch_size=1, 
                             shuffle=False,
